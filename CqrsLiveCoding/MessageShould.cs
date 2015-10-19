e8af09024a0763655088415274f591844df51908
fpellet <florent.pellet@gmail.com>
2015-10-19 15:16:27 +0200

    [Test KO] RaiseMessageQuackedWhenQuackMessage

---
+++ CqrsLiveCoding/MessageShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using Xunit;
+
+namespace CqrsLiveCoding
+{
+    public class MessageShould
+    {
+        [Fact]
+        public void RaiseMessageQuackedWhenQuackMessage()
+        {
+            var eventsStore = new List<object>();
+
+            var id = Message.Quack(eventsStore, "Hello");
+
+            Check.That(eventsStore).ContainsExactly(new MessageQuacked(id, "Hello"));
+        }
+    }
+}
