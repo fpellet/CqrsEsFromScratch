4537f8a37dc8ddd5fb9fd10189671cf18af916ec
fpellet <florent.pellet@gmail.com>
2015-10-19 15:17:04 +0200

    [Test OK] RaiseMessageQuackedWhenQuackMessage

--- CqrsLiveCoding/MessageShould.cs
+++ CqrsLiveCoding/MessageShould.cs
@@ -21,4 +21,27 @@ namespace CqrsLiveCoding
             Check.That(eventsStore).ContainsExactly(new MessageQuacked(id, "Hello"));
         }
     }
+
+    public class Message
+    {
+        public static string Quack(List<object> eventsStore, string message)
+        {
+            var id = Guid.NewGuid().ToString();
+            eventsStore.Add(new MessageQuacked(id, message));
+
+            return id;
+        }
+    }
+
+    public struct MessageQuacked
+    {
+        public string Id { get; private set; }
+        public string Message { get; private set; }
+
+        public MessageQuacked(string id, string message)
+        {
+            Id = id;
+            Message = message;
+        }
+    }
 }
