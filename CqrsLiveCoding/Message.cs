d92e93e860c0f7e047d254e9491e6aa297c8d732
fpellet <florent.pellet@gmail.com>
2016-04-05 10:02:37 +0200

    [Test KO] RaiseMessageQuackWhenQuackMessage

--- CqrsLiveCoding/Message.cs
+++ CqrsLiveCoding/Message.cs
@@ -1,6 +1,19 @@
-﻿namespace CqrsLiveCoding
+﻿using System.Collections.Generic;
+using NFluent;
+using Xunit;
+
+namespace CqrsLiveCoding
 {
     public class MessageShould
     {
+        [Fact]
+        public void RaiseMessageQuackWhenQuackMessage()
+        {
+            var history = new List<object>();
+
+            Message.Quack(history, "Hello");
+
+            Check.That(history).ContainsExactly(new MessageQuacked("Hello"));
+        }
     }
 }
