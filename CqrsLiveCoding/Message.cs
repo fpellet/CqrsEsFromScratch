b71f31d4f4eecf59707cfb137e126278f7fa596d
fpellet <florent.pellet@gmail.com>
2016-04-05 10:36:09 +0200

    IncrementWhenMessageQuacked

--- CqrsLiveCoding/Message.cs
+++ CqrsLiveCoding/Message.cs
@@ -6,6 +6,30 @@ using Xunit;
 
 namespace CqrsLiveCoding
 {
+    public class QuackCounterShould
+    {
+        [Fact]
+        public void IncrementWhenMessageQuacked()
+        {
+            var counter = new QuackCounter();
+
+            counter.When(new MessageQuacked("Hello"));
+
+            Check.That(counter.QuackNb).IsEqualTo(1);
+        }
+    }
+
+    public class QuackCounter
+    {
+        public int QuackNb { get; private set; }
+
+        public void When(MessageQuacked evt)
+        {
+            QuackNb++;
+        }
+    }
+
+    
     public class MessageShould
     {
         private readonly EventsStreamFake _eventsStream = new EventsStreamFake();
